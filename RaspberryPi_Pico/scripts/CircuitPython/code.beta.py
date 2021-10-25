1f48c7f3b5684a224fc9402606d6b323cfeb8ff2
DelinLi <delin.bio@gmail.com>
2021-10-25 15:13:38 +0800

    beta v00

--- RaspberryPi_Pico/scripts/CircuitPython/code.beta.py
+++ RaspberryPi_Pico/scripts/CircuitPython/code.beta.py
@@ -1,39 +1,48 @@
+import alarm
 import time
 import board
-from analogio import AnalogIn
- 
+from adafruit_onewire.bus import OneWireBus
+from adafruit_ds18x20 import DS18X20
+import busio
+import adafruit_tsl2561
+import adafruit_dht
+import digitalio
+import adafruit_ds3231
+import bitbangio
 
-# setup the moisture sensor power pin and turn it off by default
-#sensor_power = DigitalInOut(board.GP7)
-#sensor_power.direction = Direction.OUTPUT
-#sensor_power.value = False
-#sensor_power.value = True
+# Set an alarm for 15 seconds from now.
+time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + 900)
 
-###
-# 1. soil moisture 
-###
-# calibrate the soil moisture sensors by hand
-wet_26=300
-dry_26=600
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+led.value = True
 
-wet_27=300
-dry_27=600
+ow_bus = OneWireBus(board.GP18)
+time.sleep(0.5)
+devices = ow_bus.scan()
+ds18 = DS18X20(ow_bus, devices[0])
 
-wet_28=300
-dry_28=600
+i2c = busio.I2C(board.GP15, board.GP14)
+time.sleep(0.5)
+light = adafruit_tsl2561.TSL2561(i2c)
 
-# set the analog read pin for the moisture sensor
-sensor_signal_26 = AnalogIn(board.GP26)
-print(round(sensor_signal_26.value / 100))
-sensor_signal_27 = AnalogIn(board.GP27)
-print(round(sensor_signal_27.value / 100))
-sensor_signal_28 = AnalogIn(board.GP28)
-print(round(sensor_signal_28.value / 100))
+dht = adafruit_dht.DHT22(board.GP19)
+time.sleep(0.5)
+#time
+i2c = bitbangio.I2C(board.GP13 , board.GP12)
+ds3231 = adafruit_ds3231.DS3231(i2c)
+current = ds3231.datetime
+current_time= '{}/{}/{}\t{:02}:{:02}:{:02}'.format(current.tm_mon, current.tm_mday, current.tm_year, current.tm_hour, current.tm_min, current.tm_sec)
 
-while(True):
-    print(round(sensor_signal_26.value / 100))
-    print(round(sensor_signal_27.value / 100))
-    print(round(sensor_signal_28.value / 100))
-    print("Wait!")
-    time.sleep(6)
+recoder=current_time+"\t"+str(ds18.temperature)+"\t"+str(light.lux)+"\t"+str(dht.temperature)+"\t"+str(dht.humidity)+"\n"
+print(recoder)
+
+f = open('FieldWatcher.Pico.sensors.txt', 'a')
+f.write(recoder)
+f.close()
+led.value = False
+
+
+# Deep sleep until the alarm goes off. Then restart the program.
+alarm.exit_and_deep_sleep_until_alarms(time_alarm)
 
